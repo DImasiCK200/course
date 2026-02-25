7ee3e8da795458213b1c6a4e749bb4a35b68534b
ShpkUshnk <zgfzgfzgaf@gmail.com>
2026-02-26 01:21:11 +0700

    3.6

--- index.js
+++ index.js
@@ -2,27 +2,28 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-const generateId = () => {
-  const maxId =
-    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
-  return String(maxId + 1);
-};
+const generateId = () => String(Math.floor(Math.random() * 100000));
 
-let notes = [
+let persons = [
   {
     id: "1",
-    content: "HTML is easy",
-    important: true,
+    name: "Arto Hellas",
+    number: "040-123456",
   },
   {
     id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
   },
   {
     id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
   },
 ];
 
@@ -30,43 +31,62 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
 
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
+app.get("/info", (req, res) => {
+  const date = new Date();
+
+  res.send(
+    `<h1>Phonebook has info about ${persons.length} persons</h1><p>${date.toString()}</p>`,
+  );
 });
 
-app.get("/api/notes/:id", (req, res) => {
+app.get("/api/persons", (req, res) => {
+  res.json(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
   const id = req.params.id;
-  const note = notes.find((n) => n.id === id);
-  if (note) {
-    res.json(note);
+  const person = persons.find((n) => n.id === id);
+  if (person) {
+    res.json(person);
   } else {
     res.status(404).end();
   }
 });
 
-app.delete("/api/notes/:id", (req, res) => {
+app.delete("/api/persons/:id", (req, res) => {
   const id = req.params.id;
-  notes = notes.filter((n) => n.id !== id);
+  persons = persons.filter((n) => n.id !== id);
 
   res.status(204).end();
 });
 
-app.post("/api/notes", (req, res) => {
+app.post("/api/persons", (req, res) => {
   const body = req.body;
 
-  if (!body.content) {
-    return res.status(400).json({ error: "content missing" });
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: `name or number missing`,
+    });
   }
 
-  const note = {
-    content: body.content,
-    important: body.important || false,
+  if (
+    persons.some((p) => p.name === body.name) ||
+    persons.some((p) => p.number === body.number)
+  ) {
+    return res.status(409).json({
+      error: `entered name or number is already exist`,
+    });
+  }
+
+  const person = {
+    name: String(body.name),
+    number: String(body.number),
     id: generateId(),
   };
 
-  notes = notes.concat(note);
+  persons = persons.concat(person);
 
-  res.json(note);
+  res.json(person);
 });
 
 const PORT = 3001;
